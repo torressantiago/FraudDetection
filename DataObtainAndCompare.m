82fc71d4684544d82cb6513f7d8aa3a8cb55e1ad
Santiago Torres <47422238+torressantiago@users.noreply.github.com>
2019-09-22 17:07:53 -0500

    Added useful comments

--- DataObtainAndCompare.m
+++ DataObtainAndCompare.m
@@ -400,3 +400,234 @@ function [CardTypeNum, CardTypeList, CardTypeNumberList] = CardType5Numerized(Ca
     CardTypeNum = str2double(CardTypeNum);
     CardTypeNumberList = unique(CardTypeNum);
 end
+
+%% Visualización de datos
+% fprintf('Creando imágenes\n')
+% 
+% tic
+% 
+% % TransactionDT
+% figure
+% histogram(TransactionDTNoFraud,100)
+% hold on
+% histogram(TransactionDTIsFraud,100)
+% hold off
+% xlabel('Transaction timedelta')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Transaction timedelta')
+% 
+% % TransactionAMT
+% figure
+% histogram(TransactionAMTNoFraud,100)
+% hold on
+% histogram(TransactionAMTIsFraud,100)
+% hold off
+% xlabel('Transaction amount')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Transaction amount')
+% 
+% % ProductCD
+% [ProductCDNoFraudNum, ProducCDNoFraudList, ProductCDNoFraudNumberList] = ProductCDNumerized(ProductCDNoFraud);
+% ProducCDNoFraudList = [ProducCDNoFraudList, table2cell(table(ProductCDNoFraudNumberList))];
+% 
+% [ProductCDIsFraudNum, ProducCDIsFraudList, ProductCDIsFraudNumberList] = ProductCDNumerized(ProductCDIsFraud);
+% ProducCDIsFraudList = [ProducCDIsFraudList, table2cell(table(ProductCDIsFraudNumberList))];
+% figure
+% histogram(ProductCDNoFraudNum,100)
+% hold on
+% histogram(ProductCDIsFraudNum,100)
+% hold off
+% xlabel('Product code')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Product code')
+% 
+% % Card1 - Card6
+% figure
+% histogram(table2array(CardNoFraud(:,1)),100)
+% hold on
+% histogram(table2array(CardIsFraud(:,1)),100)
+% hold off
+% xlabel('Card 1')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Card 1')
+% 
+% figure
+% histogram(table2array(CardNoFraud(:,2)),100)
+% hold on
+% histogram(table2array(CardIsFraud(:,2)),100)
+% hold off
+% xlabel('Card 2')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Card 2')
+% 
+% figure
+% histogram(table2array(CardNoFraud(:,3)),100)
+% hold on
+% histogram(table2array(CardIsFraud(:,3)),100)
+% hold off
+% xlabel('Card 3')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Card 3')
+% 
+% 
+% [CardBrandNoFraudNum, CardBrandNoFraudList, CardBrandNoFraudNumberList] = CardBrandNumerized(CardNoFraud(:,4));
+% CardBrandNoFraudList = [CardBrandNoFraudList, table2cell(table({'0'},{'1'},{'2'},{'3'},{'4'}))'];
+% 
+% [CardBrandIsFraudNum, CardBrandIsFraudList, CardBrandIsFraudNumberList] = CardBrandNumerized(CardIsFraud(:,4));
+% CardBrandIsFraudList = [CardBrandIsFraudList, table2cell(table({'0'},{'1'},{'2'},{'3'},{'4'}))'];
+% figure
+% histogram(CardBrandNoFraudNum,5)
+% hold on
+% histogram(CardBrandIsFraudNum,5)
+% hold off
+% xlabel('Card 4')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Card 4')
+% 
+% figure
+% histogram(table2array(CardNoFraud(:,5)),100)
+% hold on
+% histogram(table2array(CardIsFraud(:,5)),100)
+% hold off
+% xlabel('Card 5')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Card 5')
+% 
+% 
+% [CardTypeNoFraudNum, CardTypeNoFraudList, CardTypeNoFraudNumberList] = CardType5Numerized(CardNoFraud(:,6));
+% CardTypeNoFraudList = [CardTypeNoFraudList, table2cell(table({'0'},{'1'},{'2'},{'3'},{'4'}))'];
+% 
+% [CardTypeIsFraudNum, CardTypeIsFraudList, CardTypeIsFraudNumberList] = CardTypeNumerized(CardIsFraud(:,6));
+% CardTypeIsFraudList = [CardTypeIsFraudList, table2cell(table({'0'},{'1'},{'2'}))'];
+% figure
+% histogram(CardTypeNoFraudNum,3)
+% hold on
+% histogram(CardTypeIsFraudNum,3)
+% hold off
+% xlabel('Card 6')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Card 6')
+% 
+% % Address
+% figure
+% histogram(AddressNoFraud(:,1),100)
+% hold on
+% histogram(AddressIsFraud(:,1),100)
+% hold off
+% xlabel('Billing Region')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Billing Region')
+% 
+% figure
+% histogram(AddressNoFraud(:,2),100)
+% hold on
+% histogram(AddressIsFraud(:,2),100)
+% hold off
+% xlabel('Billing Country')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Billing Country')
+% 
+% % Dist
+% figure
+% histogram(DistNoFraud(:,1),100)
+% hold on
+% histogram(DistIsFraud(:,1),100)
+% hold off
+% xlabel('Distance between payment address and billing address')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Distance between payment address and billing address')
+% 
+% figure
+% histogram(DistNoFraud(:,2),100)
+% hold on
+% histogram(DistIsFraud(:,2),100)
+% hold off
+% xlabel('Distance between payment address and billing address')
+% grid on
+% legend('No fradulento','Fraudulento')
+% title('Distance between payment address and billing address')
+% 
+% % P_EmailDomain
+% [P_EmailDomainNoFraudWNum, P_DomainNoFraud, P_NumberListNoFraud] = P_EmailDomainNoFraudNumerized(P_EmailDomainNoFraud);
+% P_DomainNoFraud = [P_DomainNoFraud, table2cell(table(P_NumberListNoFraud))];
+% [P_EmailDomainIsFraudWNum, P_DomainIsFraud, P_NumberListIsFraud] = P_EmailDomainIsFraudNumerized(P_EmailDomainIsFraud);
+% P_DomainIsFraud = [P_DomainIsFraud, table2cell(table(P_NumberListIsFraud))];
+% 
+% figure
+% subplot(2,1,1)
+% histogram(P_EmailDomainNoFraudWNum,60)
+% xlabel('Domain purchaser used for transaction')
+% grid on
+% title('Domain purchaser used for transaction | No Fraud')
+% subplot(2,1,2)
+% histogram(P_EmailDomainIsFraudWNum,43)
+% xlabel('Domain purchaser used for transaction')
+% grid on
+% title('Domain purchaser used for transaction | Fraud')
+% 
+% 
+% 
+% % R_EmailDomain
+% [R_EmailDomainNoFraudWNum, R_DomainNoFraud, R_NumberListNoFraud] = R_EmailDomainNoFraudNumerized(R_EmailDomainNoFraud);
+% R_DomainNoFraud = [R_DomainNoFraud, table2cell(table(R_NumberListNoFraud))];
+% [R_EmailDomainIsFraudWNum, R_DomainIsFraud, R_NumberListIsFraud] = R_EmailDomainIsFraudNumerized(R_EmailDomainIsFraud);
+% R_DomainIsFraud = [R_DomainIsFraud, table2cell(table(R_NumberListIsFraud))];
+% figure
+% subplot(2,1,1)
+% histogram(R_EmailDomainNoFraudWNum,61)
+% xlabel('Domain receiver used for transaction')
+% grid on
+% title('Domain receiver used for transaction | No Fraud')
+% subplot(2,1,2)
+% histogram(R_EmailDomainIsFraudWNum,33)
+% xlabel('Domain receiver used for transaction')
+% grid on
+% title('Domain receiver used for transaction | Fraud')
+% 
+% % C1 - C14
+% for i = 1:14
+%     figure
+%     histogram(CNoFraud(:,i),100)
+%     hold on
+%     histogram(CIsFraud(:,i),100)
+%     hold off
+%     xlabel(['C',num2str(i)])
+%     grid on
+%     legend('No fradulento','Fraudulento')
+%     title('C')
+% end
+% 
+% % D1 - D15
+% for i = 1:15
+%     figure
+%     histogram(DNoFraud(:,i),100)
+%     hold on
+%     histogram(DIsFraud(:,i),100)
+%     hold off
+%     xlabel(['D',num2str(i)])
+%     grid on
+%     legend('No fradulento','Fraudulento')
+%     title('D')
+% end
+% 
+% % M1 - M9  % Will not be plotted for resources reasons
+% 
+% 
+% % V1 - V339 % Will not be plotted for resources reasons
+% 
+% % Data from the second file will not be plotted for resource reasons
+% 
+% t(3) = toc;
+% fprintf("%d",t(3));
